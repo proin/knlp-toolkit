9c6819dd81bebd37ac9e38550dcec38ec9a3c22e
proin <proin@proinlab.com>
2016-05-04 00:18:49 +0900

    bug fixed

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -1,9 +1,9 @@
 if [ $1 = "hannanum" ]; then
     echo 'hannanum start'
-    cd /usr/local/lib/node_modules/knlp-toolket/deploy/nlp-tool/hannanum
+    cd /usr/local/lib/node_modules/knlp-toolkit/deploy/nlp-tool/hannanum
     java -jar hannanum.jar $2
 else
     echo 'kkma start'
-    cd /usr/local/lib/node_modules/knlp-toolket/deploy/nlp-tool
+    cd /usr/local/lib/node_modules/knlp-toolkit/deploy/nlp-tool
     java -jar kkma.jar $2
 fi
