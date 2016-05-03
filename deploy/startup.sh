bd41a979f975ad95101aac7aa2b59b9999e01583
proin <proin@proinlab.com>
2016-05-03 23:42:11 +0900

    [0.1.2] global path

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -4,10 +4,10 @@ echo '---';
 
 if [ $1 = "hannanum" ]; then
     echo 'hannanum start'
-    cd ./deploy/nlp-tool/hannanum
+    cd /usr/local/lib/node_modules/knlp-toolket/deploy/nlp-tool/hannanum
     java -jar hannanum.jar $2
 else
     echo 'kkma start'
-    cd ./deploy/nlp-tool
+    cd /usr/local/lib/node_modules/knlp-toolket/deploy/nlp-tool
     java -jar kkma.jar $2
 fi
